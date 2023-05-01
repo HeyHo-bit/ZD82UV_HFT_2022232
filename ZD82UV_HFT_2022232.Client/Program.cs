daad6d055ace98ceaa02426bfba9c65d941e1d46
Ádám <dm@DESKTOP-J2EVF9J>
2023-05-01 18:25:57 +0200

    Add project files.

---
+++ ZD82UV_HFT_2022232.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZD82UV_HFT_2022232.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
