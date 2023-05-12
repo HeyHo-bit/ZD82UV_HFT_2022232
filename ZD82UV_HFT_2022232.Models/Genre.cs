cc8a64fbd536d57051df650988995542d97e8423
adamzenonagy@sulid.hu <adamzenonagy@sulid.hu>
2023-05-12 23:03:43 +0200

    New content in Models

--- ZD82UV_HFT_2022232.Models/Genre.cs
+++ ZD82UV_HFT_2022232.Models/Genre.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ZD82UV_HFT_2022232.Models
 {
-    internal class Genre
+    public class Genre
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int GenreId { get; set; }
+
+        public string GenreKind { get; set; }
+
+        public int SongId { get; set; }
+        public int BandId { get; set; }
+
+        [NotMapped]
+        public virtual Song Song { get; set; }
+        
+        [NotMapped]
+        public virtual Band Band { get; set; }
     }
 }
