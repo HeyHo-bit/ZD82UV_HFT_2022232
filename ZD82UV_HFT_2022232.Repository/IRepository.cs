5b5496e8df7e504d03b8224bbe4d1ef9932164da
adamzenonagy@sulid.hu <adamzenonagy@sulid.hu>
2023-05-13 22:39:29 +0200

    Irepository created

---
+++ ZD82UV_HFT_2022232.Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZD82UV_HFT_2022232.Repository
+{
+    internal interface IRepository
+    {
+    }
+}
