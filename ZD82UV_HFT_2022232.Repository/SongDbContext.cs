9ad467dd04dc6bb356ec8fc70ffb6388e11a8515
adamzenonagy@sulid.hu <adamzenonagy@sulid.hu>
2023-05-13 19:46:47 +0200

    onConfig beta

--- ZD82UV_HFT_2022232.Repository/SongDbContext.cs
+++ ZD82UV_HFT_2022232.Repository/SongDbContext.cs
@@ -1,8 +1,27 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ZD82UV_HFT_2022232.Models;
 
 namespace ZD82UV_HFT_2022232.Repository
 {
-    public class Class1
+    public class SongDbContext: DbContext
     {
+        public DbSet<Song> Songs { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }  
+        public DbSet<Band> Bands { get; set; }
+        public DbSet<Label> Labels { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+
+            if (!builder.IsConfigured)
+            {
+                string conn = @"";
+                builder
+                    .UseLazyLoadingProxies()
+                    .UseSqlServer(conn);
+            }
+        }
     }
 }
