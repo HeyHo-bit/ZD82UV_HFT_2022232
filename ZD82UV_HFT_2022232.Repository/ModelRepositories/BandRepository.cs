5f8150f7250f9af1ab7a143b6f6cc0e603c029cd
adamzenonagy@sulid.hu <adamzenonagy@sulid.hu>
2023-05-14 13:35:14 +0200

    Repositories shorted,fixed

---
+++ ZD82UV_HFT_2022232.Repository/ModelRepositories/BandRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZD82UV_HFT_2022232.Models;
+
+namespace ZD82UV_HFT_2022232.Repository
+{
+    internal class BandRepository : Repository<Band>, IRepository<Band>
+    {
+        public BandRepository(SongDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Band Read(int id)
+        {
+            return ctx.Bands.FirstOrDefault(t => t.BandId == id);
+        }
+
+        public override void Update(Band item)
+        {
+            var old = Read(item.BandId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
