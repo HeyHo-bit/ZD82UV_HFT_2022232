c043907a8018c2a78d49827913093dc535f4b9ea
adamzenonagy@sulid.hu <adamzenonagy@sulid.hu>
2023-05-14 13:19:30 +0200

    SongRepository created

---
+++ ZD82UV_HFT_2022232.Repository/SongRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZD82UV_HFT_2022232.Models;
+
+namespace ZD82UV_HFT_2022232.Repository
+{
+    internal class SongRepository : Repository<Song>, IRepository<Song>
+    {
+        public SongRepository(SongDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Song Read(int id)
+        {
+            return ctx.Songs.FirstOrDefault(t => t.SongId == id);
+        }
+
+        public override void Update(Song item)
+        {
+            var old = Read(item.SongId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
