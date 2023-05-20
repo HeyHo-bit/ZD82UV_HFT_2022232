b7a3fe243387f34bc8b43fbcf1e2304b857079d8
adamzenonagy@sulid.hu <adamzenonagy@sulid.hu>
2023-05-20 20:00:47 +0200

    Stat controller created

---
+++ ZD82UV_HFT_2022232.Endpoint/Controllers/StatController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ZD82UV_HFT_2022232.Logic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ZD82UV_HFT_2022232.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class StatController : ControllerBase
+    {
+        ISongLogic logic;
+        IGenreLogic genreLogic;
+
+        public StatController(ISongLogic logic, IGenreLogic genreLogic)
+        {
+            this.logic = logic;
+            this.genreLogic = genreLogic;
+        }
+
+        [HttpGet]
+        public IEnumerable<YearInfo> YearStatistics(int year)
+        {
+            return this.logic.YearStatistics();
+        }
+
+        [HttpGet]
+        public IEnumerable<BestSo> BestSong()
+        {
+            return this.logic.BestSong();
+        }
+
+        [HttpGet]
+        public IEnumerable<Topla> TopLabel()
+        {
+            return this.logic.TopLabel();
+        }
+
+        [HttpGet]
+        public IEnumerable<LabelReve> LabelRevenu()
+        {
+            return this.logic.LabelRevenu();
+        }
+
+        [HttpGet]
+        public IEnumerable<MostSo> MostSong()
+        {
+            return this.genreLogic.MostSong();
+        }
+
+    }
+}
